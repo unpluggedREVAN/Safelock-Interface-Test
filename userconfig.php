48129ecd012da2a01fd61cead9fc2e10f61cdd03
Elmono56 <78763533+Elmono56@users.noreply.github.com>
2022-11-05 00:44:26 -0600

    divs changed 4

--- userconfig.php
+++ userconfig.php
@@ -9,69 +9,72 @@
     require "scripts/pi-hole/php/header.php";
 ?>
 
+<div id="principalUC">
+    
+  <div class="bodyUserC">
+    <form name="messageControl" id="messageControl" action="userconfig.php">
+      Control de Mensajería: <select name="subject" id="subject">
+          <option value="7" selected="selected">7 días</option>
+          <option value="15" selected="selected">15 días</option>
+          <option value="21" selected="selected">21 días</option>
+          <option value="30" selected="selected">30 días</option>
+          <option value="60" selected="selected">60 días</option>
+          <option value="90" selected="selected">90 días</option>
+          <option value="180" selected="selected">180 días</option>
+      </select>
+      <br> 
+      <input type="submit" value = "Confirmar">
+      </form>
+  </div>
 
-<div class="bodyUserC">
-  <form name="messageControl" id="messageControl" action="userconfig.php">
-    Control de Mensajería: <select name="subject" id="subject">
-        <option value="7" selected="selected">7 días</option>
-        <option value="15" selected="selected">15 días</option>
-        <option value="21" selected="selected">21 días</option>
-        <option value="30" selected="selected">30 días</option>
-        <option value="60" selected="selected">60 días</option>
-        <option value="90" selected="selected">90 días</option>
-        <option value="180" selected="selected">180 días</option>
-    </select>
-    <br> 
-    <input type="submit" value = "Confirmar">
+  <div class="bodyUserC">
+    <form name = "changeIPuc" id = "changeIPuc">
+      <label for="newIP">Cambiar IP a:</label><br><br>
+      <input type="text" id ="newIP" name = "newIP"><br><br> 
+      <input type="submit" value = "Confirmar">
     </form>
+  </div>
+
+  <div class="bodyUserC">
+    <h1> Control Parental </h1>
+    <label id="parentalControl" class = "label.btn.btn-success.btn-sm.toggle-on"> Enabled</label> 
+  <div>
+
+  <div class="bodyUserC">
+    <form name = "changeTheme" id = "changeTheme">
+    <h2>Cambiar Tema</h2>
+        <input type="radio" id="default" name="changeTheme" value="default">
+        <label for="default">Default</label><br>
+        <input type="radio" id="modoClaro" name="changeTheme" value="modoClaro">
+        <label for="modoClaro">Modo Claro</label><br>
+        <input type="radio" id="modoOscuro" name="changeTheme" value="modoOscuro">
+        <label for="modoOscuro">Modo Oscuro</label>
+    </form>
+  </div>
+
+  <div class="bodyUserC">
+    <form name = "changeBackGround" id = "changeBackGround">
+    <h2>Cambiar Fondo</h2>
+        <input type="radio" id="logos" name="changeBackGround" value="logos">
+        <label for="Logos">Logos</label><br>
+        <input type="radio" id="cuadrosR" name="changeBackGround" value="cuadrosR">
+        <label for="cuadrosR">Cuadros</label><br>
+        <input type="radio" id="cuadrosDel" name="changeBackGround" value="cuadrosDel">
+        <label for="cuadrosDel">Cuadros Delineados</label>
+    </form>
+  </div>
+
+  <div class="bodyUserC">
+    <form name = "ChangeTemper" id = "ChangeTemper">
+    <h2>Cambiar Unidad de Temperatura</h2>
+        <input type="radio" id="celc" name="ChangeTemper" value="celc">
+        <label for="celc">Celcious</label><br>
+        <input type="radio" id="farh" name="ChangeTemper" value="farh">
+        <label for="farh">Farenheit</label><br>
+    </form>
+  </div>
 </div>
 
-<div class="bodyUserC">
-  <form name = "changeIPuc" id = "changeIPuc">
-    <label for="newIP">Cambiar IP a:</label><br><br>
-    <input type="text" id ="newIP" name = "newIP"><br><br> 
-    <input type="submit" value = "Confirmar">
-  </form>
-</div>
-
-<div class="bodyUserC">
-  <h1> Control Parental </h1>
-  <label id="parentalControl" class = "label.btn.btn-success.btn-sm.toggle-on"> Enabled</label> 
-<div>
-
-<div class="bodyUserC">
-  <form name = "changeTheme" id = "changeTheme">
-  <h2>Cambiar Tema</h2>
-      <input type="radio" id="default" name="changeTheme" value="default">
-      <label for="default">Default</label><br>
-      <input type="radio" id="modoClaro" name="changeTheme" value="modoClaro">
-      <label for="modoClaro">Modo Claro</label><br>
-      <input type="radio" id="modoOscuro" name="changeTheme" value="modoOscuro">
-      <label for="modoOscuro">Modo Oscuro</label>
-  </form>
-</div>
-
-<div class="bodyUserC">
-  <form name = "changeBackGround" id = "changeBackGround">
-  <h2>Cambiar Fondo</h2>
-      <input type="radio" id="logos" name="changeBackGround" value="logos">
-      <label for="Logos">Logos</label><br>
-      <input type="radio" id="cuadrosR" name="changeBackGround" value="cuadrosR">
-      <label for="cuadrosR">Cuadros</label><br>
-      <input type="radio" id="cuadrosDel" name="changeBackGround" value="cuadrosDel">
-      <label for="cuadrosDel">Cuadros Delineados</label>
-  </form>
-</div>
-
-<div class="bodyUserC">
-  <form name = "ChangeTemper" id = "ChangeTemper">
-  <h2>Cambiar Unidad de Temperatura</h2>
-      <input type="radio" id="celc" name="ChangeTemper" value="celc">
-      <label for="celc">Celcious</label><br>
-      <input type="radio" id="farh" name="ChangeTemper" value="farh">
-      <label for="farh">Farenheit</label><br>
-  </form>
-</div>
 
 <?php
     require "scripts/pi-hole/php/footer.php";
